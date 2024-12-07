7434ba2a5d6c1d524e6ac5378726e7fda8fdeeb8
nicolargo <nicolashennion@gmail.com>
2024-12-07 11:28:30 +0100

    Better solution for part 1

--- day6/day06.py
+++ day6/day06.py
@@ -1,52 +1,54 @@
 def read_input(input_file):
     with open(input_file) as file:
-        return file.read().strip().split("\n")
+        ret = file.read().strip().split("\n")
+    return [list(line) for line in ret]
 
 
 def print_puzzle(puzzle):
     print("=" * len(puzzle))
-    for p in puzzle:
-        print(p)
+    for line in puzzle:
+        print("".join(line))
 
 
-def rotate_puzzle_90r(puzzle):
-    ret = [["." for _ in range(len(puzzle))] for _ in range(len(puzzle))]
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle)):
-            ret[i][j], ret[j][i] = puzzle[j][i], puzzle[i][j]
-    for row in ret:
-        row.reverse()
-    return ["".join(r) for r in ret]
+def set_puzzle(puzzle, x, y, value):
+    puzzle[y][x] = value
+
+
+def get_puzzle(puzzle, x, y):
+    return puzzle[y][x]
+
+
+def get_guard(puzzle):
+    for line_number, line in enumerate(puzzle):
+        if "^" in line:
+            return line.index("^"), line_number
+    return None, None
+
+
+def in_puzzle(puzzle, x, y):
+    return 0 <= x < len(puzzle[0]) and 0 <= y < len(puzzle)
 
 
 def count_path(puzzle):
     # Rotate the puzzle
-    puzzle = rotate_puzzle_90r(puzzle)
-    while any(["^" in i for i in puzzle]):
-        for line_number, line in enumerate(puzzle):
-            if "^" in line:
-                guard_pos = line.index("^")
-                if "#" in line[guard_pos:]:
-                    # Look for the wall
-                    wall_pos = guard_pos + line[guard_pos:].index("#")
-                    # Move the guard up to the wall
-                    # and add a X to the path
-                    puzzle[line_number] = (
-                        line[:guard_pos]
-                        + "X" * (wall_pos - guard_pos - 1)
-                        + "^"
-                        + line[wall_pos:]
-                    )
-                    # Rotate the puzzle
-                    puzzle = rotate_puzzle_90r(puzzle)
-                    puzzle = rotate_puzzle_90r(puzzle)
-                    puzzle = rotate_puzzle_90r(puzzle)
-                    break
-                else:
-                    puzzle[line_number] = line[:guard_pos] + "X" * (
-                        len(line) - guard_pos
-                    )
-                    break
+    # print_puzzle(input)
+    guard = get_guard(input)
+    guard_direction = (0, -1)
+    while True:
+        # print_puzzle(puzzle)
+        next_position = (guard[0] + guard_direction[0], guard[1] + guard_direction[1])
+        if not in_puzzle(puzzle, next_position[0], next_position[1]):
+            # Guard no more in puzzle
+            set_puzzle(puzzle, guard[0], guard[1], "X")
+            break
+        elif get_puzzle(puzzle, next_position[0], next_position[1]) in ["#", "O"]:
+            # Turn right
+            guard_direction = (-guard_direction[1], guard_direction[0])
+        else:
+            # Move forward
+            set_puzzle(puzzle, next_position[0], next_position[1], "^")
+            set_puzzle(puzzle, guard[0], guard[1], "X")
+            guard = next_position
     return sum([line.count("X") for line in puzzle])
 
 
